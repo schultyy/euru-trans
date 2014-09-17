366d218cb717f54db711975d4b0b44fcf574e1fc
Jan Schulte <hello@unexpected-co.de>
2014-09-17 22:12:07 +0200

    Show teaser text of talk

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,4 +1,12 @@
 defmodule EuruTrans.PageView do
   use EuruTrans.Views
 
+  def transcripts do
+    teaser('transcripts/rin-raeuber.md')
+  end
+
+  def teaser(name) do
+    {:ok, content} = File.read(name)
+    String.slice(content, 0, 100) <> "..."
+  end
 end
