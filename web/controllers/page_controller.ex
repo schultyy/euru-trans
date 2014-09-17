5a2948a116af1f73793853b4994137e8a2e017b6
Jan Schulte <hello@unexpected-co.de>
2014-09-18 00:38:18 +0200

    Can show complete transcript on detail page

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,7 +2,7 @@ defmodule EuruTrans.PageController do
   use Phoenix.Controller
 
   def index(conn, _params) do
-    render conn, "index", talks: transcripts
+    render conn, "index", talks: EuruTrans.Transcripts.all
   end
 
   def not_found(conn, _params) do
@@ -11,17 +11,5 @@ defmodule EuruTrans.PageController do
 
   def error(conn, _params) do
     render conn, "error"
-  end
-
-  defp transcripts do
-    {:ok, all_files} = File.ls('transcripts')
-    Enum.map all_files -- [".git", ".gitignore"], &teaser/1
-  end
-
-  defp teaser(name) do
-    {:ok, content} = File.read(Path.join('transcripts', name))
-    text = String.slice(content, 0, 100) <> "..."
-    id = String.replace(name, ".md", "")
-    %EuruTrans.Talk{speaker: "Franz", title: "Banane", text: text, id: id}
   end
 end
--- web/controllers/talk_controller.ex
+++ web/controllers/talk_controller.ex
@@ -2,6 +2,6 @@ defmodule EuruTrans.TalkController do
   use Phoenix.Controller
 
   def index(conn, %{"id" => id}) do
-    render conn, "index"
+    render conn, "index", talk: EuruTrans.Transcripts.by_id(id)
   end
 end
