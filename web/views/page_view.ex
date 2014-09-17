61f391cbc196537475eaf2301f90a31434a78546
Jan Schulte <hello@unexpected-co.de>
2014-09-17 22:47:46 +0200

    Show teaser for every talk

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -2,11 +2,12 @@ defmodule EuruTrans.PageView do
   use EuruTrans.Views
 
   def transcripts do
-    teaser('transcripts/rin-raeuber.md')
+    {:ok, all_files} = File.ls('transcripts')
+    Enum.map all_files -- [".git", ".gitignore"], &teaser/1
   end
 
   def teaser(name) do
-    {:ok, content} = File.read(name)
+    {:ok, content} = File.read(Path.join('transcripts', name))
     String.slice(content, 0, 100) <> "..."
   end
 end
