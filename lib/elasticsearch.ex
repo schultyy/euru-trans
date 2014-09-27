690bbc7675d09f201aecd2d3617df754c470e4fe
Jan Schulte <hello@unexpected-co.de>
2014-09-27 20:54:28 +0200

    Add query method to Elasticsearch module

--- lib/elasticsearch.ex
+++ lib/elasticsearch.ex
@@ -1,6 +1,10 @@
 defmodule EuruTrans.Elasticsearch do
   use HTTPotion.Base
 
+  defp query_url do
+    'http://localhost:9200/transcripts/_search'
+  end
+
   def process_request_headers(headers) do
     Dict.put headers, :"Content-Type", "application/json"
   end
@@ -8,5 +12,10 @@ defmodule EuruTrans.Elasticsearch do
   def process_response_body(body) do
     {:ok, json} = JSON.decode to_string(body)
     json["hits"]["hits"]
+  end
+
+  def query(query) do
+    start
+    post(query_url, query)
   end
 end
--- lib/transcripts.ex
+++ lib/transcripts.ex
@@ -2,14 +2,14 @@ defmodule EuruTrans.Transcripts do
   def all do
     EuruTrans.Elasticsearch.start
     {:ok, match_all} = EuruTrans.Elasticsearch.Queries.match_all |> JSON.encode
-    EuruTrans.Elasticsearch.post('http://localhost:9200/transcripts/_search', match_all).body
+    EuruTrans.Elasticsearch.query(match_all).body
     |> parse
   end
 
   def find(search_string) do
     EuruTrans.Elasticsearch.start
     {:ok, prefix} = EuruTrans.Elasticsearch.Queries.prefix("title", search_string) |> JSON.encode
-    EuruTrans.Elasticsearch.post('http://localhost:9200/transcripts/_search', prefix).body
+    EuruTrans.Elasticsearch.query(prefix).body
     |> parse
   end
 
@@ -20,7 +20,7 @@ defmodule EuruTrans.Transcripts do
   def by_id(id) do
     {:ok, id_query} = EuruTrans.Elasticsearch.Queries.ids([id]) |> JSON.encode
     EuruTrans.Elasticsearch.start
-    EuruTrans.Elasticsearch.post('http://localhost:9200/transcripts/_search', id_query).body
+    EuruTrans.Elasticsearch.query(id_query).body
     |> parse
     |> hd
   end
